a2c168b39ab48410bc5a02a97c07d86de6c22a9c
Nirav-12345 <niravpandit0002@gmail.com>
2022-03-15 00:34:04 +0530

    adding Improper method

---
+++ MSTESTUC6.3/UnitTest1.cs
@@ -0,0 +1,89 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UC6TCRETURNHAPPYMOOD
+{
+    [TestClass]
+    
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        [DataRow("I am in Happy mood")]
+        //[DataRow(null)]
+
+
+        public void GivenSadShouldReturnSad(string message)
+        {
+            string expected = "HAPPY";
+
+
+           UC6TCHAPPYMOOD mood = new UC6TCHAPPYMOOD(message);
+
+            string moo = mood.AnalyzeMood();
+
+
+            Assert.AreEqual(expected, moo);
+
+
+        }
+        [TestMethod]
+        public void GIVEN_EMPTY_MOOD_SHOULD_THROW_MOOD_ANALYZER_EXCEPTION()
+        {
+            try
+            {
+                string message = "";
+                UC6TCHAPPYMOOD mood = new UC6TCHAPPYMOOD(message);
+
+                string moo = mood.AnalyzeMood();
+            }
+            catch (MoodAnalysisException e)
+            {
+                Assert.AreEqual("Mood should not be empty", e.Message);
+            }
+        }
+        [TestMethod]
+        public void GIVEN_NULL_MOOD_SHOULD_THROW_MOOD_ANALYZER_EXCEPTION()
+        {
+            try
+            {
+                string message = null;
+                UC6TCHAPPYMOOD mood = new UC6TCHAPPYMOOD(message);
+
+                string moo = mood.AnalyzeMood();
+            }
+            catch (MoodAnalysisException e)
+            {
+                Assert.AreEqual("Mood should not be null", e.Message);
+            }
+
+
+        }
+
+        [TestMethod]
+
+        public void GivenMoodanalyseclass_Name()
+        {
+            string message = null;
+
+            object expaected = new UC6TCHAPPYMOOD(message);
+
+
+            object obj = MOODANALYZERFACTORY.InvokeAnalyseMood("MoodAnalyzer app with core.Moodanalyzer", "Mood Analyzer");
+
+            expaected.Equals(obj);
+        }
+
+
+
+        //UC6.2
+        [TestMethod]
+        public void GIvenMoodshoudreturnHappy()
+        {
+            string expected = "Happy";
+            string mood = MOODANALYZERFACTORY.InvokeAnalyseMood("Happy", "AnalyseMood");
+            Assert.AreEqual(expected, mood);
+        }
+    }
+
+
+}
---
+++ UC6.3TC/UC6TCHAPPYMOOD.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC6TCRETURNHAPPYMOOD
+{
+    public class UC6TCHAPPYMOOD
+    {
+        
+        public UC6TCHAPPYMOOD() { }
+        public string message;
+
+        public UC6TCHAPPYMOOD(string message)
+        {
+            this.message = message;
+        }
+
+
+        public string AnalyzeMood()
+        {
+
+            try
+            {
+
+
+                if (this.message.Equals(string.Empty))
+                {
+
+                    throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.Empty_Message, "Moodshoud not be empty");
+                }
+
+
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+        }
+
+
+
+
+    }
+}
+
+
+   
